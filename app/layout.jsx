2f9074730d9eb9c7567e495783f1d32f2ea9cfb8
mucahittasan <mucahittasan0@gmail.com>
2023-01-26 21:01:01 +0300

    Header and Footer components created and added to layout.js file

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,24 @@
-import './globals.css'
+import {Inter} from '@next/font/google';
+
+import '@/styles/reset.css'
+import '@/styles/global.css'
+
+const interFontFamily = Inter({subsets: ['latin']})
+
+import Header from '@/components/header'
+import Footer from '@/components/footer'
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={interFontFamily.className}>
       <head />
-      <body>{children}</body>
+      <body className="container">
+        <Header />
+          <main>
+            {children}
+          </main>
+        <Footer />
+      </body>
     </html>
   )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 
 export default function Home() {
   return (
-   <div>Merhaba gencler!</div>
+   <div>Home</div>
   )
 }
