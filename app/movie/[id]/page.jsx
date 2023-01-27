9412dd3383e0169e8598fd8aea09361bbf18cc44
mucahittasan <mucahittasan0@gmail.com>
2023-01-27 11:43:50 +0300

    movies and movie detail page created

---
+++ app/movie/[id]/page.jsx
@@ -0,0 +1,22 @@
+import { notFound } from "next/navigation";
+
+import { MovieContainer } from "@/containers/movie";
+import Movies from "@/mocks/movies.json";
+
+const MoviePage = ({ params, searchParams }) => {
+  const movieDetail = Movies.results.find(
+    (movie) => movie.id === Number(params.id)
+  );
+
+  if (!movieDetail) {
+    notFound();
+  }
+
+  if (searchParams.error === "true") {
+    throw new Error("Error happened!");
+  }
+
+  return <MovieContainer movie={movieDetail} />;
+};
+
+export default MoviePage;
